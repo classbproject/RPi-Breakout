e7e0b9dd2587a16fc4c70ad065f83cb6609da4f9
Hrishikesh <classbproject@users.noreply.github.com>
2021-08-12 09:25:23 +0530

    Update led.py

--- led.py
+++ led.py
@@ -14,42 +14,39 @@ GPIO.setup(red,GPIO.OUT)
 GPIO.setup(green,GPIO.OUT)
 GPIO.setup(blue,GPIO.OUT)
 
-print ("Testing LEDs...")
-print ("")
+print "Testing LEDs..."
+print ""
 
-print ("Green LED on")
+print "Green LED on"
 GPIO.output(green,GPIO.HIGH)
 <<<<<<< HEAD
 time.sleep(0.5)
-print ("Green LED off")
+print "Green LED off"
 =======
 time.sleep(2)
 print "Green LED off"
->>>>>>> 978f98169e09bfbe4b673642fe17972790cbb176
 GPIO.output(green,GPIO.LOW)
 
-print ("")
-print ("Red LED on")
+print ""
+print "Red LED on"
 GPIO.output(red,GPIO.HIGH)
 <<<<<<< HEAD
 time.sleep(0.5)
-print ("Red LED off")
+print "Red LED off"
 =======
 time.sleep(2)
 print "Red LED off"
->>>>>>> 978f98169e09bfbe4b673642fe17972790cbb176
 GPIO.output(red,GPIO.LOW)
 
-print ("")
-print ("Blue LED on")
+print ""
+print "Blue LED on"
 GPIO.output(blue,GPIO.HIGH)
 <<<<<<< HEAD
 time.sleep(0.5)
-print ("Blue LED off")
+print "Blue LED off"
 =======
 time.sleep(2)
 print "Blue LED off"
->>>>>>> 978f98169e09bfbe4b673642fe17972790cbb176
 GPIO.output(blue,GPIO.LOW)
 
 GPIO.cleanup()
